c0500230d403cbf0a0001ff4d9dabca6e2141485
niparra21 <niparra@estudiantec.cr>
2023-04-20 11:25:27 -0600

    proceso

--- TMatrices.py
+++ TMatrices.py
@@ -1,105 +1,62 @@
-#Reto 1
-def validarMatriz(pMatriz):
-    largoMatriz = len(pMatriz)
-    largoFila = len(pMatriz[0])
-
-    if largoFila != largoMatriz:
-        return "la matriz debe ser cuadrada"
-    largo = len(pMatriz[0])
-    for i in pMatriz:
-        if len(i) != largo:
-            return "Cada elemento de la matriz debe poseer la misma cantidad de columnas"
-        if not isinstance(pMatriz[0], list):
-            return "Cada elemento debe ser de tipo lista"
-    return True
-
-def diagonalSuperior(pMatriz):
-    validacion = validarMatriz(pMatriz)
-    if validacion != True:
-        return validacion
-    
-    for i in range(len(pMatriz)):
-        for j in range(len(pMatriz[i])):
-            if (j<i) and pMatriz[i][j] != 0:
-                return False
-    return True
-
-print(diagonalSuperior([[1,2,3],[0,12,6],[0,0,-3]]))
 #Reto 3
 
-#Reto 6
-def soloBajoDiagonal(pMatriz):
-    validacion = validarMatriz(pMatriz)
-    if validacion != True:
-        return validacion
-    
-    for i in range(len(pMatriz)):
-        for j in range(len(pMatriz[i])):
-            if ((j<i) and pMatriz[i][j] != 0) or (j>=i and pMatriz[i][j] == 0):
-                return False
-    return True
+def sumaRestaMatrices(pMatriz1,pMatriz2,pOperacion):
+    if not pOperacion=="+" and not pOperacion=="-":
+        return "La operación debe ser “+” para suma y “-” para resta."
+    elif not all(len(i)==len(pMatriz1[0]) for i in pMatriz1):
+        return "Las matrices deben ser de la misma dimensión."
+    elif not all(len(i)==len(pMatriz1[0]) for i in pMatriz2):
+        return "Las matrices deben ser de la misma dimensión."
+    elif not len(pMatriz1)==len(pMatriz2):
+        return "Las matrices deben ser de la misma dimensión."
+    elif not all(all(isinstance(i, list) and isinstance(j, int) for j in i) for i in pMatriz1):
+        return "Todos los elementos de la listas de la matriz deben de ser enteros."
+    elif not all(all(isinstance(i, list) and isinstance(j, int) for j in i) for i in pMatriz2):
+        return "Todos los elementos de la listas de la matriz deben de ser enteros."
+    filas=0
+    resultado=[]
+    while filas <= len(pMatriz1)-1:
+        columna=0
+        acumulador=[]
+        while columna <= len(pMatriz1[filas])-1:
+            if pOperacion=="+":
+                acumulador.append((pMatriz1[filas][columna])+(pMatriz2[filas][columna]))
+            else:
+                acumulador.append((pMatriz1[filas][columna])-(pMatriz2[filas][columna]))
+            columna+=1
+        resultado.append(acumulador)
+        filas+=1
+    return resultado
 
-print(soloBajoDiagonal([[1,2,3],[1,12,1],[0,0,-3]]))
+print(sumaRestaMatrices([[1,2],[3,6]],[[5,6],[7,8]],"-"))
+
 #Reto 7
 
-#Reto 8
-def sumaFila(pLista):
-    resultado = 0
-    for i in pLista:
-        resultado+=i
+def girarDerecha(pMatriz):
+    resultado=[]
+    columna=0
+    while columna <= len(pMatriz[1])-1:
+        fila=0
+        i=1
+        acumulador=[]
+        while fila <= len(pMatriz)-1:
+            acumulador.append(pMatriz[len(pMatriz)-i][columna])
+            i+=1
+            fila+=1
+        resultado.append(acumulador)
+        columna+=1
     return resultado
 
-def sumaColumna(pMatriz, pCol):
-    resultado = 0
-    for i in pMatriz:
-        resultado += i[pCol]
-    return resultado
+def girarIzquierda(pMatriz):
+    return girarDerecha(girarDerecha(girarDerecha(pMatriz)))
 
-def matrizMagica(pMatriz):
-    validacion = validarMatriz(pMatriz)
-    if validacion != True:
-        return validacion
-
-    resultados = []
-    diagonal = 0
-    diagInversa = 0
-    for i in range(len(pMatriz)):
-        resultados.append(sumaFila(pMatriz[i]))
-        for j in range(len(pMatriz[i])):
-            resultados.append(sumaColumna(pMatriz, j))
-            if i==j:
-                diagonal += pMatriz[i][j]
-            if i == len(pMatriz[i])-1-j:
-                diagInversa += pMatriz[i][j]
-    resultados += [diagonal, diagInversa]
-    if all(x == resultados[0] for x in resultados):
-        return resultados[0]
+def girarMatriz(pMatriz,pDireccion):
+    if pDireccion=="d":
+       return girarDerecha(pMatriz)
     else:
-        return "No es una matriz mágica"
-print(matrizMagica([[2,7,6], [9,5,1], [4,3,8]]))
+        return girarIzquierda(pMatriz)
 
-#Reto 9
-def validarNotas(pMatriz):
-    if pMatriz == []:
-        return "Matriz vacia"
-    for notas in pMatriz:
-        if len(notas) != 8:
-            return "la matriz debe tener 8 columnas"
-    return True
     
-def promedio(pLista):
-    resultado = 0
-    for i in pLista:
-        resultado+=i
-    return resultado/len(pLista)
 
-def analizarMatrizNotas(pMatriz):
-    validacion = validarNotas(pMatriz)
-    if not validacion==True:
-        return validacion
-    analisis = []
-    for notas in pMatriz:
-        analisis.append([promedio(notas), min(notas), max(notas), "Aprobado" if promedio(notas) >=70 else "Reprobado"])
-    return analisis
-
-print(analizarMatrizNotas([[90,12,90,89,89,89,89,89],[90,67,78,89,89,89,89,89],[0,0,0,0,0,0,0,0]]))
+print(girarMatriz([[1,2,3,4],[5,6,7,8],[9,10,11,12]],"I"))     
+    
